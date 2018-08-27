c78a4243b560acb6f6092b961ffa83208188a07d
1shoot101kill <21776744@student.uwa.edu.au>
2018-08-27 22:05:04 +0800

    Android
    Add questionire activity

--- SleepDiary/app/src/main/java/com/example/zihaol/sleepdiary/MainActivity.java
+++ SleepDiary/app/src/main/java/com/example/zihaol/sleepdiary/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.zihaol.sleepdiary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button submit_button;
+    private EditText code_edit;
+    private String Code;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+    private void initView(){
+        submit_button=(Button)findViewById(R.id.btn_login);
+
+
+        submit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
