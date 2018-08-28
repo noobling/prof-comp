d39f91fe06de183c94944cb15e597937fc97eb6d
1shoot101kill <21776744@student.uwa.edu.au>
2018-08-28 21:55:32 +0800

    Android 8.28
    Update Questionire layout and add style sheet

--- SleepDiary/app/src/main/java/com/example/zihaol/sleepdiary/MainActivity.java
+++ SleepDiary/app/src/main/java/com/example/zihaol/sleepdiary/MainActivity.java
@@ -1,16 +1,20 @@
 package com.example.zihaol.sleepdiary;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button submit_button;
     private EditText code_edit;
     private String Code;
+    private Context content;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,12 +24,18 @@ public class MainActivity extends AppCompatActivity {
     }
     private void initView(){
         submit_button=(Button)findViewById(R.id.btn_login);
+        code_edit=(EditText)findViewById(R.id.code) ;
 
 
         submit_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Code=code_edit.getText().toString();
+                content=MainActivity.this;
+                Intent intent = new Intent(content, Questionire.class);
+                intent.putExtra("code", Code);
+               // intent.putExtra("username", "this is user name...");
+                Toast.makeText(MainActivity.this, "Welcome "+Code, Toast.LENGTH_SHORT).show();
             }
         });
     }
