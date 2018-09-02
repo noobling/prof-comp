9faa847d3cf116f2e00968a65a305e20c8d6c57a
0x0 <ghstoreinvesting@gmail.com>
2018-09-02 20:27:43 +0800

    Update the landing page

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,16 +1,29 @@
 <template>
-  <v-card>
-    <v-card-text >
-      <v-layout row wrap justify-center align-center>
-        <v-flex xs12>
-          <h1 class="display-4">Building the next generation of web apps</h1>
-        </v-flex>
-        <v-flex xs12>
-          <v-btn large color="primary">Get Started</v-btn>
+ <v-jumbotron
+    :gradient="gradient"
+    dark
+    src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg"
+    height= "100%"
+  >
+    <v-container fill-height>
+      <v-layout align-center>
+        <v-flex text-xs-center>
+          <h3 class="display-2 mb-4">Center for Sleep Science Sleep Diary App</h3>
+          <v-btn large outline to="/login">Get Started</v-btn>
         </v-flex>
       </v-layout>
-    </v-card-text>
-  </v-card>
+    </v-container>
+  </v-jumbotron>
 </template>
 
-
+<script>
+  export default {
+    layout: 'main',
+    head() {
+      return { title: 'Sleep Diary App' }
+    },
+    data: () => ({
+      gradient: 'to top right, rgba(63,81,181, .7), rgba(25,32,72, .7)'
+    })
+  }
+</script>
