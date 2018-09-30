1ae9857069c3221607483d4e86b600af15ffcfbd
0x0 <ghstoreinvesting@gmail.com>
2018-09-30 21:43:27 +0800

    Add support for ie using babel poly fill

--- website/client/nuxt.config.js
+++ website/client/nuxt.config.js
@@ -86,6 +86,7 @@ module.exports = {
     /*
     ** You can extend webpack config here
     */
+    vendor: ['babel-polyfill'],
     extend(config, ctx) {
       // Run ESLint on save
       if (ctx.isDev && ctx.isClient) {
