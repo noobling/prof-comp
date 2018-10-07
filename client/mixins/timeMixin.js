52f178ccb8cfbb267bcc06fad31aaad557bb390f
0x0 <ghstoreinvesting@gmail.com>
2018-10-07 12:05:54 +0800

    Update time sheet and improve menu button

--- client/mixins/timeMixin.js
+++ client/mixins/timeMixin.js
@@ -18,7 +18,7 @@ export default {
             let timeHoursStr = timeHours.toString().padStart(2, '0')
             timeText = timeHoursStr + ":" + timeMinutesStr + prefix
           }
-          let quickFindText = timeText.replace(/\s/g, '').replace(':00', '').toLowerCase()
+          let quickFindText = timeText.replace(/\s/g, '').replace(':00', '').replace(':', '').toLowerCase()
           quickFindText = quickFindText[0] === '0' ? quickFindText.substring(1): quickFindText
           const timeObj = {
             time: timeText, 
@@ -27,6 +27,40 @@ export default {
           }
           timesArr.push(timeObj)
           timeMinutes += 15
+        }
+        timeHours += 1
+        timeMinutes = 0
+      }
+      return timesArr.reverse()
+    },
+
+    timeOfDayArrLong: function () {
+      let timeMinutes = 0 // Start at 12 am
+      let timeHours = 0 // Start at 12 am
+      let timesArr = []
+      while (timeHours != 24) {
+        while (timeMinutes != 60) {
+          let timeText = null
+          let prefix = timeHours > 11 ? " PM" : " AM"
+          let timeMinutesStr = timeMinutes.toString().padStart(2, '0')
+          if (timeHours == 0) {
+            timeText = "12:"+ timeMinutesStr + prefix
+          } else if (timeHours > 12) {
+            let timeHoursStr = (timeHours - 12).toString().padStart(2, '0')
+            timeText = timeHoursStr + ":" + timeMinutesStr + prefix
+          } else {
+            let timeHoursStr = timeHours.toString().padStart(2, '0')
+            timeText = timeHoursStr + ":" + timeMinutesStr + prefix
+          }
+          let quickFindText = timeText.replace(/\s/g, '').replace(':00', '').replace(':', '').toLowerCase()
+          quickFindText = quickFindText[0] === '0' ? quickFindText.substring(1): quickFindText
+          const timeObj = {
+            time: timeText, 
+            value: timeHours.toString().padStart(2, '0') + ":" + timeMinutesStr,
+            quickFindText
+          }
+          timesArr.push(timeObj)
+          timeMinutes += 1
         }
         timeHours += 1
         timeMinutes = 0
